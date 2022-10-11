bd366ae83cdbe75223079f56ee24519d94062b3e
inamuu <8310973+inamuu@users.noreply.github.com>
2022-10-11 18:35:25 +0900

    Add a function that get my projects

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
 
 load_dotenv(verbose=True)
 
@@ -10,8 +11,22 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 
+def get_projects(api_url: dict):
+    payload = {
+        "Authorization": "Basic " + os.environ.get("API_TOKEN")
+    }
+    response = requests.get(
+        api_url + "/me",
+        headers=payload
+    ).json()
+    return response
+
+
 def main():
-    print(os.environ.get("CIRCLECI_API_V1"))
+    response = get_projects(os.environ.get("CIRCLECI_API_V1"))
+    if type(response) != dict:
+        raise TypeError('Not dict')
+    project_list = response['projects']
 
 
 if __name__ == '__main__':
